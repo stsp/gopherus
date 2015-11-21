ca907653b4c5498b8e9b3eeb88fb72df6e4eac87
mv_fox <mv_fox@767d67df-c2ba-418b-950d-eab06a56d38d>
2015-11-21 18:33:39 +0000

    when dealing with integers that can go higher than 65535, then a long must be used instead of int
    git-svn-id: https://svn.code.sf.net/p/gopherus/code/trunk@18 767d67df-c2ba-418b-950d-eab06a56d38d

--- int2str.c
+++ int2str.c
@@ -6,28 +6,29 @@
 #include <stdlib.h>  /* NULL, div() */
 
 /* converts an integer from the range 0..9999999 into a string. returns the length of the computed string, or -1 on error. */
-int int2str(char *res, int x) {
-  int step, reslen = 0;
+int int2str(char *res, long x) {
+  long step;
+  int reslen = 0;
   div_t divres;
   if (res == NULL) return(-1);
-  if ((x < 0) || (x > 9999999)) {
+  if ((x < 0) || (x > 9999999l)) {
     *res = 0;
     return(-1);
   }
   if (x < 10) {
     step = 1;
-  } else if (x < 100) {
+  } else if (x < 100l) {
     step = 10;
-  } else if (x < 1000) {
+  } else if (x < 1000l) {
     step = 100;
-  } else if (x < 10000) {
+  } else if (x < 10000l) {
     step = 1000;
-  } else if (x < 100000) {
+  } else if (x < 100000l) {
     step = 10000;
-  } else if (x < 1000000) {
-    step = 100000;
+  } else if (x < 1000000l) {
+    step = 100000l;
   } else {
-    step = 1000000;
+    step = 1000000l;
   }
   for (; step > 0; step /= 10) {
     divres = div(x, step);  /* all this could be written as: */
--- int2str.h
+++ int2str.h
@@ -6,5 +6,5 @@
 #ifndef int2str_h_sentinel
   #define int2str_h_sentinel
   /* converts an integer from the range 0..9999999 into a string. returns the length of the computed string, or -1 on error. */
-  int int2str(char *res, int x);
+  int int2str(char *res, long x);
 #endif
