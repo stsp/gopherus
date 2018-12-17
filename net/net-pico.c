d393be32657cedd4ad389cbc0f0d31cd69fbcfb8
mv_fox <mv_fox@767d67df-c2ba-418b-950d-eab06a56d38d>
2018-12-17 18:44:49 +0000

    replaced the gcc-8 warning work-around by a simple comment, read https://developers.redhat.com/blog/2017/03/10/wimplicit-fallthrough-in-gcc-7/ for details
    git-svn-id: https://svn.code.sf.net/p/gopherus/code/trunk@50 767d67df-c2ba-418b-950d-eab06a56d38d

--- net/net-pico.c
+++ net/net-pico.c
@@ -9,9 +9,12 @@
  */
 
 #include <stdlib.h>
-#include <string.h>
-#include <limits.h>
-#include <errno.h>
+#include <time.h>
+#include <i86.h>
+
+#include <pico_stack.h>
+#include <pico_dns_client.h>
+#include <picodos.h>
 
 #include "net.h" /* include self for control */
 
@@ -20,17 +23,57 @@
 #define verboseMode 0
 
 
+static struct pico_device *picodev;
+static volatile int dnswait = 0;
+static volatile char *dnsres = NULL;
 
-
-/* this is a wrapper around the wattcp lookup_host(), but with a small integrated cache.
-   returns 0 if resolutin fails. */
-unsigned long net_dnsresolve(const char *name) {
-  return(100l);
+/* callback for pico_dns_client_getaddr() */
+static void cb_dns(char *addr, void *arg) {
+  dnsres = addr;
+  dnswait = 0;
 }
 
+/* this is a wrapper around the wattcp lookup_host(), but with a small integrated cache.
+   returns 0 if resolution fails. */
+unsigned long net_dnsresolve(const char *name) {
+  unsigned long res;
+  int crudetimer;
+  time_t t;
+  pico_dns_client_getaddr(name, cb_dns, NULL);
+  /* wait for the callback to provide resolution, or time out */
+  t = time(NULL);
+  crudetimer = 0;
+  dnswait = 1;
+  while (dnswait) {
+    union REGS regs;
+    int86(0x28, &regs, &regs); /* DOS 2+ IDLE INTERRUPT */
+    pico_stack_tick();
+    if (time(NULL) != t) {
+      t = time(NULL);
+      crudetimer++;
+      if (crudetimer > 3) return(0);
+    }
+  }
+  /* convert string to long, free dnsres and return to caller */
+  pico_string_to_ipv4((char *)dnsres, &res);
+  free((char *)dnsres);
+  return(res);
+}
+
+static void freepico(void) {
+  picoquit(picodev);
+  free(picodev);
+}
 
 /* must be called before using libtcp. returns 0 on success, or non-zero if network subsystem is not available. */
 int net_init(void) {
+  picodev = malloc(sizeof(struct pico_device));
+  if (picodev == NULL) return(-1);
+  if (picoinit(picodev, 0) != 0) {
+    free(picodev);
+    return(-2);
+  }
+  atexit(freepico);
   return(0);
 }
 
