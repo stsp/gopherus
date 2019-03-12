bdd382ad985b00c24a205f062a67f3704a06448b
mv_fox <mv_fox@767d67df-c2ba-418b-950d-eab06a56d38d>
2019-03-12 11:03:07 +0000

    bookmarks reading implementation for Windows
    git-svn-id: https://svn.code.sf.net/p/gopherus/code/trunk@160 767d67df-c2ba-418b-950d-eab06a56d38d

--- fs/fs-win.c
+++ fs/fs-win.c
@@ -2,3 +2,18 @@
  * This file is part of the Gopherus project.
  * Copyright (C) 2019 Mateusz Viste
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#include "fs.h"
+
+/* returns path and filename of the bookmark file */
+char *bookmarks_getfname(void) {
+  static char r[512];
+  snprintf(r, sizeof(r), "%s/Gopherus", getenv("APPDATA"));
+  CreateDirectory(r, NULL);
+  snprintf(r, sizeof(r), "%s/Gopherus/gopherus.bkm", getenv("APPDATA"));
+  return(r);
+}
