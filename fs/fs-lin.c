c8b7f641393c73014fcf561ce1340547f2c607d1
mv_fox <mv_fox@767d67df-c2ba-418b-950d-eab06a56d38d>
2020-01-18 21:34:16 +0000

    stupid tweak to shut clang/gcc compile warning
    git-svn-id: https://svn.code.sf.net/p/gopherus/code/trunk@200 767d67df-c2ba-418b-950d-eab06a56d38d

--- fs/fs-lin.c
+++ fs/fs-lin.c
@@ -12,7 +12,7 @@
 /* returns path and filename of the bookmark file */
 char *bookmarks_getfname(const char *argv0) {
   static char r[512];
-  argv0 = argv0; /* for gcc to shut up */
+  if (argv0 == NULL) r[0] = 0; /* stupid thing for gcc to shut up */
   snprintf(r, sizeof(r), "%s/.gopherus.bookmarks", getenv("HOME"));
   return(r);
 }
